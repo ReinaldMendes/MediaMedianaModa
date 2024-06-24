73454684737015ae3f77e7344d6235c2feffd715
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-06-24 10:23:12 -0300

    ok

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let notas = [10.0, 9.0, 9.5, 7.3, 7.8, 8.0];
+let notas = [10, 9.0, 9.5, 7.3, 7.8, 8.0];
 let resultado = 0;
 for (let i = 0; i < notas.length; i++) {
   resultado = resultado + notas[i];
