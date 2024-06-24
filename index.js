c2d80a5892e799bfd7e453212b32e9a6e3bf6f0f
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-06-24 11:04:38 -0300

    padronizado

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-
 let notas = [10, 9.0, 9.5, 7.3, 7.8, 8.0];
 let resultado = 0;
 for (let i = 0; i < notas.length; i++) {
   resultado = resultado + notas[i];
 }
-console.log(resultado / notas.length);
+console.log("Media = " + resultado / notas.length);
 
 //media
-
 
 let frequencia = {};
 let moda = [];
@@ -36,16 +34,15 @@ console.log("Moda:", moda);
 let notasOrdem = [];
 for (let i = 0; i < notas.length; i++) {
   for (let j = i + 1; j < notas.length; j++) {
-    if (notas [i] > notas[j]) {
+    if (notas[i] > notas[j]) {
       let aux = notas[i];
-       notas[i] = notas[j];
-        notas[j] = aux;
+      notas[i] = notas[j];
+      notas[j] = aux;
     }
   }
 }
-console.log(notas);
 notasOrdem.push(notas);
-console.log(notasOrdem);
+
 for (let y = 0; y < notasOrdem.length; y++) {
   if (y == 0) {
     let posicao = notasOrdem[y].length;
@@ -53,13 +50,13 @@ for (let y = 0; y < notasOrdem.length; y++) {
     posicao = posicao % 2 == 0;
 
     if (posicao === true) {
-      let calculo0 = notasOrdem[y].length /2
-      let calculo2=  calculo -1;
-      let calculo=notasOrdem[calculo0]+notasOrdem[calculo1]/2
-      console.log(calculo);
+      let calculo0 = notasOrdem[y].length / 2;
+      let calculo2 = calculo0 - 1;
+      let calculo = notasOrdem[calculo0] + notasOrdem[calculo2] / 2;
+      console.log("Mediana" + calculo);
     } else if (posicao == false) {
       let medianaImpar = notasOrdem[y][3];
-      console.log(medianaImpar);
+      console.log("Mediana" + medianaImpar);
     }
   }
 }
